79981383fa4a5223323f6c92be3e3a8aebfb792f
Theodore Summer <TWSummer@gmail.com>
2018-02-20 09:39:32 -0800

    Env variables are correctly rendering content

--- main.go
+++ main.go
@@ -5,12 +5,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/joho/godotenv"
 )
 
 var r *gin.Engine
@@ -20,13 +14,6 @@ func main() {
 	r, db = router.SetupRouterAndDB()
 	r.Static("/css", "backend/css")
 	defer db.Close()
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	secret := os.Getenv("SECRET_KEY")
-	fmt.Printf("Secret Key is %v", secret)
 
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
