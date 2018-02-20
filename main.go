32a1f961bde2f3d929f26130aa2acbefe15bb71f
Theodore Summer <TWSummer@gmail.com>
2018-02-20 09:32:33 -0800

    Create env file and print variable

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 var r *gin.Engine
@@ -14,6 +20,13 @@ func main() {
 	r, db = router.SetupRouterAndDB()
 	r.Static("/css", "backend/css")
 	defer db.Close()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	secret := os.Getenv("SECRET_KEY")
+	fmt.Printf("Secret Key is %v", secret)
 
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
