64bf65e222a2deddafe4af8c52ea2d831fdb1881
Theodore Summer <TWSummer@gmail.com>
2018-02-20 12:10:13 -0800

    Store initialized and added to window

--- frontend/xperience_city.jsx
+++ frontend/xperience_city.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  let store = configureStore();
+  window.store = store;
   ReactDOM.render(<h1>Welcome to Xperience City</h1>, root);
 });
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import configureStore from './store/store';
-// import Root from './components/root';
-//
-// document.addEventListener("DOMContentLoaded", () => {
-//   const root = document.getElementById("root");
-//   let store = configureStore();
-//   ReactDOM.render(<Root store={store} />, root);
-// });
