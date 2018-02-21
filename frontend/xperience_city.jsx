1f990916899a16c24f234e32deced2d93acfd54f
Patrick <patrick.chao.14@gmail.com>
2018-02-20 23:06:07 -0800

    Changed event listener for DOMContentLoaded to listener for Auth response from Facebook

--- frontend/xperience_city.jsx
+++ frontend/xperience_city.jsx
@@ -4,38 +4,45 @@ import configureStore from './store/store';
 import {checkLoginState} from './actions/session_actions';
 import Root from './components/root';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
-  let store = configureStore();
-  //Required for facebook auth;
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=867019043470476&autoLogAppEvents=1';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
 
-  //Required for bootstrapping user
-  window.fbAsyncInit = function() {
-    console.log("init!");
-    FB.init({
-      appId      : '867019043470476',
-      status     : true,
-      cookie     : true,  // enable cookies to allow the server to access
-                          // the session
-      xfbml      : true,  // parse social plugins on this page
-      version    : 'v2.8' // use graph api version 2.8
-    });
+//Adds Facebook's SDK to our script tags. Wish we could load it manually...
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=867019043470476&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
-    //Subscribe to a crazy event that facebook provides
-    FB.Event.subscribe('auth.statusChange', function(response) {
-      if(response.status == 'connected') {
-        store.dispatch(checkLoginState());
-      }
-    });
+//This method is called in the window once Facebook has been loaded in our script tags
+window.fbAsyncInit = function() {
+  console.log("init!");
+  FB.init({
+    appId      : '867019043470476',
+    status     : true,
+    cookie     : true,  // enable cookies to allow the server to access
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.8' // use graph api version 2.8
+  });
 
-  };
-  window.store = store;
-  ReactDOM.render(<Root store={store} />, root);
+  //Subscribe to a crazy event that facebook provides, instead of DOMContentLoaded
+  FB.Event.subscribe('auth.statusChange', function(response) {
+    const root = document.getElementById('root');
+    let store = configureStore();
+    //Required for facebook auth;
+
+
+    //Required for bootstrapping user
+    if(response.status == 'connected') {
+      store.dispatch(checkLoginState());
+    }
+    window.store = store;
+    ReactDOM.render(<Root store={store} />, root);
+  });
+
+};
+
+document.addEventListener('DOMContentLoaded', (response) => {
+
 });
