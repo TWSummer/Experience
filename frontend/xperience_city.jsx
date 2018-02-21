e12ccc1aabc14da6b418aeaefc6bc1bb46025173
Theodore Summer <TWSummer@gmail.com>
2018-02-20 20:42:07 -0800

    Render root component into xperience_city.jsx

--- frontend/xperience_city.jsx
+++ frontend/xperience_city.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   let store = configureStore();
   window.store = store;
-  ReactDOM.render(<h1>Welcome to Xperience City</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
