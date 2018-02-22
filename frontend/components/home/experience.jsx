6820690267f4ca73d2166ce921a7b5743eae7c86
Theodore Summer <TWSummer@gmail.com>
2018-02-21 16:53:59 -0800

    Add vote button so experience component

--- frontend/components/home/experience.jsx
+++ frontend/components/home/experience.jsx
@@ -8,8 +8,14 @@ class Experience extends React.Component {
    render() {
      return (
        <div className="experience-index-item">
-         <p>{this.props.experience.Title}</p>
-         <section className="experience-ribbon"></section>
+         <aside className="vote-buttons">
+           <i class="fas fa-angle-up"></i>
+           <i class="fas fa-angle-down"></i>
+         </aside>
+         <section>
+           <p>{this.props.experience.Title}</p>
+           <section className="experience-ribbon"></section>
+         </section>
        </div>
      );
    }
