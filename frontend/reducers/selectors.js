2e7d3fbca142f7e2eeb1b0adf3ce0d21616162a3
Theodore Summer <TWSummer@gmail.com>
2018-02-24 16:17:20 -0800

    Modify selector to prevent errors when experience has no activities

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -2,13 +2,13 @@ export const singleExperience = (state, ownProps) => {
   if (state.entities.experiences[ownProps.match.params.id]) {
     let activitiesArr = [];
     let experience = state.entities.experiences[ownProps.match.params.id];
-    Object.keys(experience.Activities).forEach((id) => {
-      activitiesArr.push(experience.Activities[id]);
-    });
-    experience.activities = activitiesArr;
-    console.log(experience);
-    return experience;
-  } else {
-    return null;
+    if (experience.Activities) {
+      Object.keys(experience.Activities).forEach((id) => {
+        activitiesArr.push(experience.Activities[id]);
+      });
+      experience.activities = activitiesArr;
+      return experience;
+    }
   }
+  return null;
 };
