256ff9261512ecd8f6ab8637993bc71b433662c4
Theodore Summer <TWSummer@gmail.com>
2018-03-13 15:25:12 -0700

    Add handleScroll event to home page

--- frontend/components/home/home_page.jsx
+++ frontend/components/home/home_page.jsx
@@ -4,10 +4,18 @@ import SmallExperienceContainer from '../small_experience/small_experience_conta
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
+    this.handleScroll = this.handleScroll.bind(this);
   }
 
   componentWillMount() {
     this.props.fetchExperiences(10, 0);
+  }
+
+  handleScroll(e) {
+    let heightBound = window.height * 0.8;
+    if (heightBound > window.scrollY) {
+        this.props.fetchExperiences(10, this.props.experiences.length);
+    }
   }
 
    render() {
@@ -18,7 +26,7 @@ class HomePage extends React.Component {
            <img className="banner-image"
              src="https://i.imgur.com/O72fdnu.jpg" />
          </header>
-         <section>
+         <section onScroll={this.handleScroll}>
            {
              this.props.experiences.map((experience) => {
                return (
